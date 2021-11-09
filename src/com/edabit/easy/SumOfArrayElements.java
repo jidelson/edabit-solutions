57fac83fbc7cb0d184aa50250e96aab3b2871ccb
jidelson <joeidelson@gmail.com>
2021-11-09 15:14:48 -0800

    Get the Sum of All Array Elements

---
+++ src/com/edabit/easy/SumOfArrayElements.java
@@ -0,0 +1,11 @@
+package com.edabit.easy;
+
+public class SumOfArrayElements {
+	  public static int arraySum(int[] arr) {
+			int sum = 0;
+			for(int i = 0; i<arr.length; i++){
+				sum += arr[i];
+			}
+			return sum;
+		}
+}
