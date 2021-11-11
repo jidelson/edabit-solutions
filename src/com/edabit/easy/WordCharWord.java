deb45e68a1e145e6befce05abe128bdd6df3e71c
jidelson <joeidelson@gmail.com>
2021-11-10 23:23:18 -0800

    WordCharWord

---
+++ src/com/edabit/easy/WordCharWord.java
@@ -0,0 +1,17 @@
+package com.edabit.easy;
+
+// WordCharWord
+
+
+// Create a function that will put the first argument, a character, between every word in the second argument, a string.
+
+
+
+
+public class WordCharWord {
+	
+	public static String add(String c, String str) {
+		return str.replace(" ", c);
+	}
+
+}
