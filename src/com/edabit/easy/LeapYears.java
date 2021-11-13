560e9963f85649601782f95ba387a46333841763
jidelson <joeidelson@gmail.com>
2021-11-12 22:13:14 -0800

    Leap Years

---
+++ src/com/edabit/easy/LeapYears.java
@@ -0,0 +1,35 @@
+package com.edabit.easy;
+
+// Leap Years
+
+
+// A leap year has one day added to February for being synchronized with the seasonal year. A leap year appears with a regular 
+// frequency, which is determined by the rule below:
+
+// A year must either be divisible by 400 or divisible by 4 and not 100.
+// Given a year you must implement a function that returns true if it's a leap year, or false if it's not.
+
+
+//Examples
+//isLeap(2020) -> true
+//// Exactly divided by 4 and not by 100.
+//
+//isLeap(1800) -> false
+//// Exactly divided by 4, but is also exactly divided by 100.
+//
+//isLeap(2000) -> true
+//// Exactly divided by 400.
+//
+//isLeap(2019) -> false
+//// It can't be exactly divided by 400 or by 4.
+
+
+public class LeapYears {
+	
+	public static boolean isLeap(int year) {
+		if((year % 400 == 0 ) || ((year % 4 == 0) && (year % 100 != 0))){
+			return true;
+		} else return false;
+  }
+
+}
